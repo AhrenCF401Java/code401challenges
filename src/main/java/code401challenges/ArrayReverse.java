aa139dcca56e84ec514bf85a80f640a99832a3b9
ahren <ahrenswett@gmail.com>
2019-09-17 17:09:46 -0700

    fix

--- src/main/java/code401challenges/ArrayReverse.java
+++ src/main/java/code401challenges/ArrayReverse.java
@@ -3,8 +3,8 @@ public class ArrayReverse{
     
     public static void main(String[]args){
         int[] llama = new int[]{1,2,3,4,5,6};
-        int[] arr2 = reverse(llama);
-        System.out.println(Arrays.toString(arr2));
+        llama = reverse(llama);
+        System.out.println(Arrays.toString(llama));
     }
     public static int[] reverse(int[] array){
             int[] reverseArray = new int[array.length];

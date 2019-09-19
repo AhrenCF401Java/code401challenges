fb7ac3ba04ad00b459b388081f27befef23d7511
ahren <ahrenswett@gmail.com>
2019-09-18 20:28:58 -0700

    need readme made 2 tests and created the binary search

---
+++ src/main/java/code401challenges/BinarySearch.java
@@ -0,0 +1,25 @@
+package code401challenges;
+
+public class BinarySearch {
+    public static void main(String[]args){
+
+    }
+
+    public static int search(int[] arr,int target) {
+        int length = arr.length;
+        int l = 0;
+        int r = length - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (arr[mid] < target) {
+                l = mid + 1;
+            } else if (arr[mid] > target) {
+                r = mid - 1;
+            } else {
+                return mid;
+            }
+
+        }
+        return -1;
+    }
+}

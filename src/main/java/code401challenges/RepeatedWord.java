7b0f5f1bc4e87865d0a5f0a3b00604b7fb18a1f3
ahren <ahrenswett@gmail.com>
2019-10-29 10:38:03 -0700

    added readme

--- src/main/java/code401challenges/RepeatedWord.java
+++ src/main/java/code401challenges/RepeatedWord.java
@@ -5,15 +5,13 @@ import code401challenges.Hashtable.Hashtable;
 public class RepeatedWord {
     public String dupWords(String txt){
         Hashtable txtTable = new Hashtable();
+//        split each
         for(String word : txt.split(" ")){
-            int key = 0;
-            String keyString=""+key;
-            word.replace("[^a-zA-Z0-9","");
+            word = word.replace("[^a-zA-Z0-9","").toLowerCase();
             if (txtTable.contains(word)){
                 return word;
             }else{
-                txtTable.add(keyString, word);
-                key++;
+                txtTable.add(word, word);
             }
         }
         return null;
---
+++ src/main/java/code401challenges/readmes/repeatedWord.md
@@ -0,0 +1,18 @@
+# Repeated Word
+
+##
+##Problem Domain
+return a word if a duplicate is found in a given string
+
+#
+##Solution
+Add all words to the hash map without special characters or capitalization
+and check if it is in the table before adding it, if it is oin the table return the word
+## Approach & Efficiency
+O(1)
+##Whiteboard pics:
+<img src="../assets/repeatedWords.jpg">
+
+##
+
+[Return to all README's](../README.md)

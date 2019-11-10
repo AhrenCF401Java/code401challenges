46f03b04f5dcead99f5ae6adeff3237b4bfdc14d
ahren <ahrenswett@gmail.com>
2019-11-10 11:36:05 -0800

    fixed preDepth added tests

--- src/main/java/code401challenges/graph/PreOrderDepthTraversal.java
+++ src/main/java/code401challenges/graph/PreOrderDepthTraversal.java
@@ -1,22 +1,29 @@
 package code401challenges.graph;
 
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
 public class PreOrderDepthTraversal {
 
-    Set<Node> seen = new HashSet<>();
+    public static <T> List<T> preDepth(Graph<T> graph, Node<T> node){
+        Set<Node<T>> seen = new HashSet<>();
+        List<T> values = new LinkedList<>();
 
-    public Set preDepth(Graph graph, Node node){
+        return preDepth(graph, node, seen, values);
+    }
+
+    public static <T> List<T> preDepth(Graph<T> graph, Node<T> node, Set<Node<T>> seen, List<T> values){
         if (node != null) {
             if (seen.add(node)) {
-                List<Edge> neighbs = graph.getNeighbors(node);
+                values.add(node.value);
+                List<Edge<T>> neighbs = graph.getNeighbors(node);
                 for (Edge neighb : neighbs) {
-                    preDepth(graph, neighb.nodePointingTo);
+                    preDepth(graph, neighb.nodePointingTo, seen, values);
                 }
             }
         }
-        return seen;
+        return values;
     }
 }
--- src/test/java/code401challenges/graph/PreOrderDepthTraversalTest.java
+++ src/test/java/code401challenges/graph/PreOrderDepthTraversalTest.java
@@ -1,8 +1,72 @@
 package code401challenges.graph;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class PreOrderDepthTraversalTest {
+    @Test
+    public void preDepthTest() {
+        Graph<String>testGraph = new Graph<>();
+        Node Pandora = testGraph.add("Pandora");
+        Node Metroville = testGraph.add("Metroville");
+        Node Arendelle = testGraph.add("Arendelle");
+        Node Monstropolis = testGraph.add("Monstropolis");
+        Node Naboo = testGraph.add("Naboo");
+        Node Narnia = testGraph.add("Narnia");
+        testGraph.addEdge(Pandora,Arendelle,150);
+        testGraph.addEdge(Arendelle,Naboo,82);
+        testGraph.addEdge(Arendelle,Narnia,99);
+        testGraph.addEdge(Pandora,Monstropolis,105);
+        testGraph.addEdge(Monstropolis,Metroville,73);
 
 
+        List<String> places = PreOrderDepthTraversal.preDepth(testGraph, Pandora);
+        List<String> expectedPlaces = new LinkedList<>();
+        expectedPlaces.add("Pandroa");
+        expectedPlaces.add("Arendelle");
+        expectedPlaces.add("Naboo");
+        expectedPlaces.add("Narnia");
+        expectedPlaces.add("Monstropolis");
+        expectedPlaces.add("Metroville");
+        assertEquals(expectedPlaces,places);
+
+
+
+        expectedPlaces.add("Naboo");
+        assertNotEquals(expectedPlaces,places);
+
+    }
+    @Test
+    public void preDepthTest2() {
+        Graph<String>testGraph = new Graph<>();
+        Node Pandora = testGraph.add("Pandora");
+        Node Metroville = testGraph.add("Metroville");
+        Node Arendelle = testGraph.add("Arendelle");
+        Node Monstropolis = testGraph.add("Monstropolis");
+        Node Naboo = testGraph.add("Naboo");
+        Node Narnia = testGraph.add("Narnia");
+        testGraph.addEdge(Pandora,Arendelle,150);
+        testGraph.addEdge(Pandora, Naboo,82);
+        testGraph.addEdge(Arendelle,Narnia,99);
+        testGraph.addEdge(Pandora,Monstropolis,105);
+        testGraph.addEdge(Monstropolis,Metroville,73);
+
+
+        List<String> places = PreOrderDepthTraversal.preDepth(testGraph, Pandora);
+        List<String> expectedPlaces = new LinkedList<>();
+        expectedPlaces.add("Pandora");
+        expectedPlaces.add("Arendelle");
+        expectedPlaces.add("Narnia");
+        expectedPlaces.add("Naboo");
+        expectedPlaces.add("Monstropolis");
+        expectedPlaces.add("Metroville");
+
+        assertEquals(expectedPlaces,places);
+
+    }
 }
